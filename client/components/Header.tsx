6d51c222bf305fe18390fc60b81fcb83b37b2730
Siddhartha Roy <sid86harth@gmail.com>
2022-06-13 08:37:48 +0000

    Header Component

---
+++ client/components/Header.tsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react'
+
+const Header = () => {
+    return (
+        <div>Header s</div>
+    )
+}
+export default Header;
---
+++ studio/schemas/userSchema.js
@@ -0,0 +1,29 @@
+export const userSChema = {
+    name: 'users',
+    title: 'Users',
+    type: 'document',
+    fields: [
+        {
+            name: 'address',
+            title: 'Wallet Address',
+            type: 'string'
+        },
+        {
+            name: 'userName',
+            title: 'User Name',
+            type: 'string'
+        },
+        {
+            name: 'transactions',
+            title: 'Transactions',
+            type: 'array',
+            of: [
+                {
+                    type: 'reference',
+                    to: [{ type: 'transactions' }]
+                }
+            ]
+        },
+
+    ]
+}
