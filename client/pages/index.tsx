c094efb9f5c642842ade78bf210053a86e95e5fc
Siddhartha Roy <sid86harth@gmail.com>
2022-06-13 10:13:06 +0000

    integrate metamask

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import { Main } from '../components/Main'
 
 const style = {
   wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`
@@ -13,7 +14,7 @@ const Home: NextPage = () => {
         <title>Uniswap Clone</title>
       </Head>
       <Header />
-      <h2>Main</h2>
+      <Main />
       <h2>Transaction History</h2>
     </div>
   )
