9713604fdef43ad833c91bd68f7c219dcfa90a00
Siddhartha Roy <sid86harth@gmail.com>
2022-06-14 09:14:57 +0000

    save data to db

--- client/context/TransactionContext.js
+++ client/context/TransactionContext.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { contractABI, contractAddress } from '../lib/constants'
 import { ethers } from 'ethers'
+import { client } from '../lib/sanityClient'
 
 export const TransactionContext = React.createContext()
 
@@ -35,6 +36,21 @@ export const TransactionProvider = ({ children }) => {
     useEffect(() => {
         checkIfWallterIsConnected()
     }, [])
+
+    // create new user profile in db
+    useEffect(() => {
+        if (!currentAccount) return
+            ; (async () => {
+                const userDoc = {
+                    _type: 'users',
+                    _id: currentAccount,
+                    userName: 'Unnamed',
+                    address: currentAccount,
+                }
+
+                await client.createIfNotExists(userDoc)
+            })()
+    }, [currentAccount])
 
     const connectWallet = async (metamask = eth) => {
         try {
@@ -99,12 +115,13 @@ export const TransactionProvider = ({ children }) => {
 
             await transactionHash.wait()
 
-            // await saveTransaction(
-            //     transactionHash.hash,
-            //     amount,
-            //     connectedAccount,
-            //     addressTo
-            // )
+            // db
+            await saveTransaction(
+                transactionHash.hash,
+                amount,
+                connectedAccount,
+                addressTo
+            )
 
             setIsLoading(false);
         }
@@ -112,16 +129,50 @@ export const TransactionProvider = ({ children }) => {
             console.error(error)
         }
 
-    }
+    };
 
     const handleChange = (e, name) => {
         setFormData((prevState) => ({
             ...prevState, [name]: e.target.value
         }))
-    }
+    };
+
+    const saveTransaction = async (
+        txHash,
+        amount,
+        fromAddress = currentAccount,
+        toAddress,
+    ) => {
+        const txDoc = {
+            _type: 'transactions',
+            _id: txHash,
+            fromAddress: fromAddress,
+            toAddress: toAddress,
+            timestamp: new Date(Date.now()).toISOString(),
+            txHash: txHash,
+            amount: parseFloat(amount),
+        }
+
+        await client.createIfNotExists(txDoc)
+
+        await client
+            .patch(currentAccount)
+            .setIfMissing({ transactions: [] })
+            .insert('after', 'transactions[-1]', [
+                {
+                    _key: txHash,
+                    _ref: txHash,
+                    _type: 'reference',
+                },
+            ])
+            .commit()
+
+        return
+    };
+
 
     return (
-        < TransactionContext.Provider
+        <TransactionContext.Provider
             value={{
                 currentAccount,
                 connectWallet,
